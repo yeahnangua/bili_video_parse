8869a46012a8dc2c1faba84e21b4b41796222e76
yeahnangua <1135746060@qq.com>
2024-07-10 13:57:18 +0800

    update

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 logging.basicConfig(filename='app.log', level=logging.DEBUG)
 
 # Bilibili 配置
-bilibili_cookie = ""  # 请在这里填入你的 Bilibili Cookie
+bilibili_cookie = "buvid3=AEFFCF15-C2E9-793B-491D-008BF0C90F7181101infoc; b_nut=1708509581; i-wanna-go-back=-1; b_ut=7; _uuid=A45D2A41-E510A-7825-6D71-583425B31DB260284infoc; buvid4=61A85774-CD3B-69A8-C2BC-924829AB4E1582108-024022109-wOq%2F528XLFNoA5mFMDlDvw%3D%3D; buvid_fp=2cf80278a68c3a68b477341ad2413cc4; header_theme_version=CLOSE; DedeUserID=305501959; DedeUserID__ckMd5=4be9a350f49fb1fc; rpdid=|(YYYYYRmuY0J'u~|)l|m)u); enable_web_push=ENABLE; iflogin_when_web_push=1; fingerprint=48be66a35f4e388b3068541117bcbd81; CURRENT_FNVAL=4048; LIVE_BUVID=AUTO8517086005952981; PVID=3; home_feed_column=5; browser_resolution=1482-706; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDkxMjgxODksImlhdCI6MTcwODg2ODkyOSwicGx0IjotMX0.nXakkRyuJ6CmUHwKVMq8B_oK2IkAm5CLNaE3SqVD3Go; bili_ticket_expires=1709128129; SESSDATA=59cbf0df%2C1724557449%2C8be7e%2A22CjDmTFA_A9qpH15TbD83W33LB9hnLq-MS_4XGl5eTQ0o3desbTUFp7qMxFGtrFlVTDESVlJWV193dlBpNVlSZG96LTIzYXZFbkQ3SzVOYUFyeFBPYXE0ME9XUTlSdVphUl9MU0oxYUR0WmUwLWVqVUZWdWxXYlpFNnBwLWFwU0tjOTJLUHFpMFRnIIEC; bili_jct=5e8563e6de2034002aebe8fec18a76e9; sid=6vsco01x; CURRENT_QUALITY=120; bp_video_offset_305501959=903138786663202832"  # 请在这里填入你的 Bilibili Cookie
 bilibili_headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 OPR/107.0.0.0',
     'Cookie': bilibili_cookie,
@@ -17,18 +17,7 @@ bilibili_headers = {
     'referer': 'https://www.bilibili.com'
 }
 
-# 网易云音乐配置
-wwy_cookie=""
-netease_headers = {
-    'Cookie': bilibili_cookie,
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
-}
-migu_head={
-            'referer':'http://m.music.migu.cn',
-            'proxy':'true',
-            'Cookie':"migu_cookie_id=8102c5b3-74c9-4092-8682-61e4cbef3153-n41709986285789; mg_uem_user_id_9fbe6599400e43a4a58700a822fd57f8=27106c8a-acc5-47aa-af4d-95d4570712b4; cookieId=iPWZd2ObI1NT7NzaqVORmtegzcqhph71709986342312; idmpauth=true@passport.migu.cn; migu_music_status=true; migu_music_uid=91181714096; migu_music_avatar=; migu_music_nickname=%E5%92%AA%E5%92%95%E9%9F%B3%E4%B9%90%E7%94%A8%E6%88%B7; migu_music_level=0; migu_music_credit_level=1; migu_music_platinum=0; migu_music_msisdn=nCSbdmuLOmYoBiolJwqzkQ%3D%3D; migu_music_email=; migu_music_passid=609049552082860128; migu_music_sid=s%3A0V4rf7nZtA_ro-VcIu9-JqvD4bsiRjLS.lbOBKMsN9FAmmawcHKFHDPvqLiZdbXYr1qGDQ45e8KE; player_stop_open=0; playlist_adding=1; addplaylist_has=1; audioplayer_new=1; add_play_now=1; audioplayer_open=1; playlist_change=0; audioplayer_exist=1; WT_FPC=id=2b8d1e7b5f77a5e95011709986286725:lv=1710040781466:ss=1710040754751",
-            'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
-            }
+
 
 
 # 解析 Bilibili 视频链接，提取 BV 号
@@ -69,34 +58,6 @@ def get_video_direct_url(bv):
         return None
 
 
-# 网易云音乐解析逻辑（示例，需要实际实现）
-def netease_music_handler(url):
-    # 这里应实现对网易云音乐歌曲ID的解析或歌曲名搜索，以及获取下载链接的逻辑
-    # 由于实现细节可能涉及版权问题，这里仅提供框架代码
-    # 歌曲url
-    url = 'https://m.music.migu.cn/migu/remoting/scr_search_tag?keyword={}&type={}&pgc={}&rows={}'.format(url, 2,
-
-                                                                                                         1, 1)
-    proxies = {
-        "http": "http://118.25.84.151:8888",
-        "https": "http://118.25.84.151:8888",
-    }
-    req = requests.get(url, headers=migu_head, timeout=5,proxies=proxies)
-    print(req)
-    app.logger.info(req)
-    print(req.json())
-    url=req.json()["musics"][0]["mp3"]
-    print(url)
-    app.logger.info(url)
-    mp3_index = url.find(".mp3")
-
-    # 截取从开始到 ".mp3"（包含）为止的所有内容
-    if mp3_index != -1:
-        trimmed_url = url[:mp3_index + 4]  # 加4是因为".mp3"有4个字符，我们想包含这四个字符
-    else:
-        trimmed_url = url  # 如果找不到".mp3"，则返回原始字符串
-
-    return trimmed_url
 
 
 
@@ -157,19 +118,7 @@ def index():
                 print("\n\n\n")
                 app.logger.info("\n\n\n")
                 return 'Failed to get live direct link', 500
-        else:
-            # 处理网易云音乐链接或搜索歌曲名
-            print(url)
-            app.logger.info(url)
-            download_url = netease_music_handler(url)
-            if download_url:
-                print("success")
-                app.logger.info("success")
-                print("\n\n\n")
-                app.logger.info("\n\n\n")
-                return redirect(download_url)
-            else:
-                return 'Failed to get music download link', 500
+
 
     else:
         return send_from_directory('static', 'no_video.html')
