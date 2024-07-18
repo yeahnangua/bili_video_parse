ce1e301c375e3bba96fbb28fb2efea767341be85
yeahnangua <1135746060@qq.com>
2024-07-18 22:17:50 +0800

    update

--- app.py
+++ app.py
@@ -85,8 +85,9 @@ def music_hander(url):
     print(music_id)
     req = requests.get(f"https://music.tongxuewen.me/mv/url?id={music_id}",headers=music_headers)
     print(req)
-    print(req.json())
-    music_url = req.json()['data'][0]['url']
+    reqq=req.json()
+    print(reqq)
+    music_url = reqq['data']['url']
     print(music_url)
     return music_url
 
