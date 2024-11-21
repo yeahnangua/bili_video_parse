204663ce4710f9ecb93c9b08eac7791af0496691
yeahnangua <1135746060@qq.com>
2024-11-21 17:21:44 +0800

    优化log，去除重复的探针log输出

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 logging.basicConfig(filename='app.log', level=logging.DEBUG)
 
 # Bilibili 配置
-bilibili_cookie = "buvid3=AEFFCF15-C2E9-793B-491D-008BF0C90F7181101infoc; b_nut=1708509581; i-wanna-go-back=-1; b_ut=7; _uuid=A45D2A41-E510A-7825-6D71-583425B31DB260284infoc; buvid4=61A85774-CD3B-69A8-C2BC-924829AB4E1582108-024022109-wOq%2F528XLFNoA5mFMDlDvw%3D%3D; buvid_fp=2cf80278a68c3a68b477341ad2413cc4; header_theme_version=CLOSE; DedeUserID=305501959; DedeUserID__ckMd5=4be9a350f49fb1fc; rpdid=|(YYYYYRmuY0J'u~|)l|m)u); enable_web_push=ENABLE; iflogin_when_web_push=1; fingerprint=48be66a35f4e388b3068541117bcbd81; CURRENT_FNVAL=4048; LIVE_BUVID=AUTO8517086005952981; PVID=3; home_feed_column=5; browser_resolution=1482-706; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDkxMjgxODksImlhdCI6MTcwODg2ODkyOSwicGx0IjotMX0.nXakkRyuJ6CmUHwKVMq8B_oK2IkAm5CLNaE3SqVD3Go; bili_ticket_expires=1709128129; SESSDATA=59cbf0df%2C1724557449%2C8be7e%2A22CjDmTFA_A9qpH15TbD83W33LB9hnLq-MS_4XGl5eTQ0o3desbTUFp7qMxFGtrFlVTDESVlJWV193dlBpNVlSZG96LTIzYXZFbkQ3SzVOYUFyeFBPYXE0ME9XUTlSdVphUl9MU0oxYUR0WmUwLWVqVUZWdWxXYlpFNnBwLWFwU0tjOTJLUHFpMFRnIIEC; bili_jct=5e8563e6de2034002aebe8fec18a76e9; sid=6vsco01x; CURRENT_QUALITY=120; bp_video_offset_305501959=903138786663202832"  # 请在这里填入你的 Bilibili Cookie
+bilibili_cookie = "buvid3=AEFFCF15-C2E9-793B-491D-008BF0C90F7181101infoc; b_nut=1708509581; i-wanna-go-back=-1; b_ut=7; _uuid=A45D2A41-E510A-7825-6D71-583425B31DB260284infoc; buvid4=61A85774-CD3B-69A8-C2BC-924829AB4E1582108-024022109-wOq%2F528XLFNoA5mFMDlDvw%3D%3D; buvid_fp=2cf80278a68c3a68b477341ad2413cc4; header_theme_version=CLOSE; DedeUserID=305501959; DedeUserID__ckMd5=4be9a350f49fb1fc; rpdid=|(YYYYYRmuY0J'u~|)l|m)u); enable_web_push=ENABLE; iflogin_when_web_push=1; fingerprintt=48be66a35f4e388b3068541117bcbd81; CURRENT_FNVAL=4048; LIVE_BUVID=AUTO8517086005952981; PVID=3; home_feed_column=5; browser_resolution=1482-706; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDkxMjgxODksImlhdCI6MTcwODg2ODkyOSwicGx0IjotMX0.nXakkRyuJ6CmUHwKVMq8B_oK2IkAm5CLNaE3SqVD3Go; bili_ticket_expires=1709128129; SESSDATA=59cbf0df%2C1724557449%2C8be7e%2A22CjDmTFA_A9qpH15TbD83W33LB9hnLq-MS_4XGl5eTQ0o3desbTUFp7qMxFGtrFlVTDESVlJWV193dlBpNVlSZG96LTIzYXZFbkQ3SzVOYUFyeFBPYXE0ME9XUTlSdVphUl9MU0oxYUR0WmUwLWVqVUZWdWxXYlpFNnBwLWFwU0tjOTJLUHFpMFRnIIEC; bili_jct=5e8563e6de2034002aebe8fec18a76e9; sid=6vsco01x; CURRENT_QUALITY=120; bp_video_offset_305501959=903138786663202832"  # 请在这里填入你的 Bilibili Cookie
 bilibili_headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 OPR/107.0.0.0',
     'Cookie': bilibili_cookie,
@@ -46,14 +46,14 @@ def get_fenp_from_url(url):
                             p_value = param[2:]
                             if "&" in p_value:
                                 p_value = p_value.split("&")[0]
-                            print(p_value)
-                            print("获取到分p")
+                            printt(p_value)
+                            printt("获取到分p")
                             return int(p_value)
         else:
-            print("URL不是Bilibili视频链接")
+            printt("URL不是Bilibili视频链接")
             return None
     except Exception as e:
-        print(f"解析分P时发生错误: {e}")
+        printt(f"解析分P时发生错误: {e}")
         return None
 
 
@@ -68,15 +68,15 @@ def get_video_direct_url(bv,p):
     pagelist_url = f"https://api.bilibili.com/x/player/pagelist?bvid={bv}"
     pagelist_response = requests.get(pagelist_url, headers=bilibili_headers)
     if pagelist_response.status_code == 200:
-        print("pagelist返回200")
+        printt("pagelist返回200")
         try:
             pagelist_data = pagelist_response.json()
-            #print(pagelist_data)
-            #print(p)
+            #printt(pagelist_data)
+            #printt(p)
             cid = pagelist_data['data'][p-1]['cid']
-            print(cid)
+            printt(cid)
         except Exception as e:
-            print(f"!!!!!\nException: {str(e)} {bv} {pagelist_data}\n!!!")
+            printt(f"!!!!!\nException: {str(e)} {bv} {pagelist_data}\n!!!")
             return None
     else:
         return None
@@ -106,28 +106,35 @@ def live_handler(url):
                            headers=bilibili_headers).json()['data']['room_id']
     live_url = requests.get(f"https://api.live.bilibili.com/room/v1/Room/playUrl?cid={room_id}&platform=h5",
                             headers=bilibili_headers).json()['data']['durl'][0]['url']
-    print(live_url)
+    printt(live_url)
     app.logger.info(live_url)
     return live_url
 
 def music_hander(url):
     music_id = get_music_from_url(url)
-    print(music_id)
+    printt(music_id)
     req = requests.get(f"https://music.tongxuewen.me/mv/url?id={music_id}",headers=music_headers)
-    print(req)
+    printt(req)
     reqq=req.json()
-    print(reqq)
+    printt(reqq)
     music_url = reqq['data']['url']
-    print(music_url)
+    printt(music_url)
     return music_url
 
+
+def printt(a):
+    if(tz == False): print(a)
 
 
 @app.route('/')
 def index():
+    global tz
+    tz=False
     url = request.args.get('url')
-    print(url)
-    print(datetime.datetime.now())
+    if(url == 'https://www.bilibili.com/video/BV1Jm411D7WQ/?spm_id_from=333.1007.tianma.1-1-1.click'):
+        tz=True
+    printt(url)
+    printt(datetime.datetime.now())
     app.logger.info(datetime.datetime.now())
     if url:
         if "bilibili.com/video/" in url:
@@ -136,44 +143,44 @@ def index():
             if bv:
                 direct_url = get_video_direct_url(bv,fenp)
                 if direct_url:
-                    print("success")
+                    printt("success")
                     app.logger.info("success")
-                    print("\n\n\n")
+                    printt("\n\n\n")
                     app.logger.info("\n\n\n")
                     return redirect(direct_url)
                 else:
-                    print("Failed to get video direct link")
+                    printt("Failed to get video direct link")
                     app.logger.info("Failed to get video direct link")
-                    print("\n\n\n")
+                    printt("\n\n\n")
                     app.logger.info("\n\n\n")
                     return 'Failed to get video direct link', 500
             else:
-                print("Invalid Bilibili video URL")
+                printt("Invalid Bilibili video URL")
                 app.logger.info("Invalid Bilibili video URL")
-                print("\n\n\n")
+                printt("\n\n\n")
                 app.logger.info("\n\n\n")
                 return 'Invalid Bilibili video URL', 400
         if "live.bilibili.com" in url:
-            print(url)
+            printt(url)
             app.logger.info(url)
             live_url = live_handler(url)
             if live_url:
-                print("success")
+                printt("success")
                 app.logger.info("success")
-                print("\n\n\n")
+                printt("\n\n\n")
                 app.logger.info("\n\n\n")
                 return redirect(live_url)
             else:
-                print("Failed to get LIVE direct link")
+                printt("Failed to get LIVE direct link")
                 app.logger.info("Failed to get LIVE direct link")
-                print("\n\n\n")
+                printt("\n\n\n")
                 app.logger.info("\n\n\n")
                 return 'Failed to get live direct link', 500
         if "mv?id" in url:
-            print(url+"123123")
+            printt(url+"123123")
             app.logger.info(url)
             music_url = music_hander(url)
-            print(music_url)
+            printt(music_url)
             return redirect(music_url)
 
 
